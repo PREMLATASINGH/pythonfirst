00787963284fbf31d94b9897bda05984c8389d8b
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-15 15:50:05 -0500

    added numpy

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
 print("hello world")
+print(" I am premlata singh")
+import pandas as pd
+print("pandas version:", pd.__version__)
+import numpy as np
+print("numpy version:", np.__version__)
+arr=np.array([1, 2, 3, 4, 5])
+print("numpy array:", arr)
+arr1=arr*2
+print("array multiplied by 2:", arr1)
