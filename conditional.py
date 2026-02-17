9b65a8fe16f758a736f0797256c73bf27acc7303
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-17 15:09:05 -0500

    added mody

--- conditional.py
+++ conditional.py
@@ -9,6 +9,11 @@ else:
 b=int(input("Enter a number: "))
 if b>=18:
     print("You are an adult.")
-else:    print("You are a minor.")  
-
-      
+else:    print("You are a minor.") 
+age=20
+if age<13:
+    print("You are a child.")
+elif age<20:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
