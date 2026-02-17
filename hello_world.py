8546072479f66addc6316f307db73aa7e9ce9a48
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-17 06:28:29 -0500

    added condition

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,10 @@
 print("hello world")
 print(" I am premlata singh")
-import pandas as pd
-print("pandas version:", pd.__version__)
-import numpy as np
-print("numpy version:", np.__version__)
-arr=np.array([1, 2, 3, 4, 5])
+##import pandas as pd
+##print("pandas version:", pd.__version__)
+##import numpy as np
+##print("numpy version:", np.__version__)
+"""arr=np.array([1, 2, 3, 4, 5])
 print("numpy array:", arr)
 arr1=arr*2
 print("array multiplied by 2:", arr1)
@@ -25,7 +25,7 @@ print("sum of the array:", arr7)
 a=np.array([[1, 2], [3, 4]])
 b=np.array([[5, 6], [7, 8]])    
 c=np.dot(a, b)
-print("dot product of a and b:", c)
+##print("dot product of a and b:", c)
 arr8=arr2.size
 print("size of the array:", arr8)
 arr9=arr2.shape     
@@ -39,16 +39,22 @@ print("Hello, " + name + "!")
 person={"name": name, "age": 30, "city": "New York City"}
 print("Person details:", person)
 number=[1, 2, 3, 4, 5]
-print(number[0])
-def greet(name):
-    return "Hello, " + name + "!"   
-greeting=greet(name)
-print(greeting)
-print("This is a simple Python program that demonstrates basic operations with NumPy arrays, user input, and functions.")
-for i in range(5):
-    print("Iteration:", i)
+print(number[0])"""
 
 a=5
 b=10
 sum=a+b 
 print("The sum of a and b is:", sum)
+x=int(input("Enter a number: "))
+if x>0:
+    print("The number is positive.")
+elif x<0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+if x%2==0:
+    print("The number is even.")    
+else:    print("The number is odd.")
+if x%3==0:
+    print("The number is divisible by 3.")  
