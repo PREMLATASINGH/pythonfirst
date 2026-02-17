be65eb079c390326eef0b715b35a5646b8c5f98f
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-17 15:39:01 -0500

    added

--- loop.py
+++ loop.py
@@ -14,4 +14,9 @@ while count<5:
 count=0
 while count%2==0:   
     print(count)
-    count+=1    
+    count+=1   
+count=0
+while count<10:
+    if count%2==0:
+        print(count)
+    count+=1 
