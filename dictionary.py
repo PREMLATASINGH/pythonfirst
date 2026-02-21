33a8e400e97d757ccba47cf9313d2dbfbdd45722
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-21 15:35:34 -0500

    added

--- dictionary.py
+++ dictionary.py
@@ -66,3 +66,5 @@ cubes={x:x**3 for x in range(1,6)}
 print(cubes)
 even={x:x*2 for x in range(1,11) if x%2==0}
 print(even)
+even_squares={x:x**2 for x in range(1,11) if x%2==0}
+print(even_squares)
