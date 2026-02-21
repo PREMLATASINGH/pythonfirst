535b34b2d1af4f9dbec78407807b64f11cd2985c
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-21 15:31:54 -0500

    added

--- dictionary.py
+++ dictionary.py
@@ -60,3 +60,5 @@ for student_id, student_info in students.items():
     print(f"{student_id}: {student_info}")
     for key, value in student_info.items():
         print(f"  {key}: {value}")
+squares={x:x**2 for x in range(1,6)}
+print
