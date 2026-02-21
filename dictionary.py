1e489e29eea8e45f4526af8e584818e0fa3de71f
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-21 15:25:12 -0500

    added

--- dictionary.py
+++ dictionary.py
@@ -53,3 +53,4 @@ students={"student1":{"name":"Alice","age":20,"major":"Computer Science"},
 print(students)
 print(students["student1"])
 print(students["student1"]["name"])
+print(students["student2"])
