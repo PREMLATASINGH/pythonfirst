2c2e80c44abb66a5fb548ab07607d09ab1da1233
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-27 18:05:43 -0400

    data set

--- pandas1.py
+++ pandas1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = {"name": ["A", "B"], "age": [20, 30]}
+data = {"name": ["A", "B","c"], "age": [20, 30,40]}
 df = pd.DataFrame(data)
 print(df)
 print(type(df))
@@ -9,3 +9,4 @@ print(df.info())
 print(df.describe())
 print(df.isnull().sum())
 print(df.fillna(0))
+print(df)
