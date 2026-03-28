c7af03f5c21d8d8e46b6009097ac0193747bb549
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-28 00:12:47 -0400

    added

--- pro1.py
+++ pro1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 df=pd.read_csv('E-commerce .csv')
 print(df)
 print(df.head())
@@ -8,5 +9,5 @@ print(df.info())
 print(df.isnull().sum())
 print(df.fillna(0))
 print(type(df))
-df['total_sales']=df['Price']*df['Qantity']
-print(df['total_sales'])
+df['total_sale']=np.sum(['Sales'])
+print(df['total_sale'])
