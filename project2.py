9b51a7eb366a4f3683cc6f5ce947662d8e7aea9e
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-30 15:42:56 -0400

    total sales

--- project2.py
+++ project2.py
@@ -4,7 +4,7 @@ data={"product":["laptop","phone","chair","table","laptop",'airpot','table'],
     "category":["tech","tech","furniture","furniture","tech",'tech',"furniture"],
     "quantity":[1,2,3,1,2,7,4],
     "price":[1200,800,150,300,1200,500,675],
-    "cost":[900,500,100,200,900,700,800],
+    "sales":[900,500,100,200,900,700,800],
     "region":["east","west","east","south","west","east","west"],
     "date":['2024-01-01','2024-01-02','2024-01-03','2024-01-04','2024-01-01','2024-01-05','2024-01-06']
 }
@@ -17,3 +17,5 @@ print(df.tail())
 print(df.shape)
 print(df.columns)
 print(df.isnull().sum())
+total_sales = df['sales'].sum()
+print(total_sales)
