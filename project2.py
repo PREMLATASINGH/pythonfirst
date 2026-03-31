60f69a1f6649ee7d7de9128b1ed60e8393fc56ea
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-31 11:38:44 -0400

    column

--- project2.py
+++ project2.py
@@ -56,4 +56,5 @@ print(df7)
 df8=df.groupby('category')['total_sales'].agg(['sum','mean','max'])
 print(df8)"""
 print(df.columns)
-
+df = df.rename(columns={'sales': 'total_sales'})
+print(df.columns)
